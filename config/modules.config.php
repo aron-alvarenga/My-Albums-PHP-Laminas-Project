4136c485e99bbd4aa7e531f89f103244ae1f9d33
aron-alvarenga <aron.alvarenga@hotmail.com>
2023-03-26 10:54:57 -0400

    Modules

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,14 +6,15 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
-    'Laminas\Mvc\I18n',
-    'Laminas\Form',
-    'Laminas\I18n',
-    'Laminas\InputFilter',
-    'Laminas\Filter',
-    'Laminas\Hydrator',
-    'Laminas\Db',
-    'Laminas\Router',
-    'Laminas\Validator',
-    'Application',
+  'Laminas\Mvc\I18n',
+  'Laminas\Form',
+  'Laminas\I18n',
+  'Laminas\InputFilter',
+  'Laminas\Filter',
+  'Laminas\Hydrator',
+  'Laminas\Db',
+  'Laminas\Router',
+  'Laminas\Validator',
+  'Application',
+  'Album',
 ];
